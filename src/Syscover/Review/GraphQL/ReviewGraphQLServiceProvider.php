33b1a01b5e87ed964570634c0cecc05da0ab8e23
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-11-30 12:24:55 +0100

    build

---
+++ src/Syscover/Review/GraphQL/ReviewGraphQLServiceProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Syscover\Review\GraphQL;
+
+use GraphQL;
+
+class ReviewGraphQLServiceProvider
+{
+    public static function bootGraphQLTypes()
+    {
+
+    }
+
+    public static function bootGraphQLSchema()
+    {
+
+    }
+}
---
+++ src/Syscover/Review/ReviewServiceProvider.php
@@ -0,0 +1,48 @@
+<?php namespace Syscover\Market;
+
+use Illuminate\Support\ServiceProvider;
+use Syscover\Review\GraphQL\ReviewGraphQLServiceProvider;
+
+class ReviewServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        // register routes
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+
+        // register migrations
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+
+        // register translations
+        $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'review');
+
+        // register seeds
+        $this->publishes([
+            __DIR__ . '/../../database/seeds/' => base_path('/database/seeds')
+        ], 'seeds');
+
+        // register config files
+        $this->publishes([
+            __DIR__ . '/../../config/pulsar-review.php' => config_path('pulsar-review.php'),
+        ]);
+
+        // register GraphQL types and schema
+        ReviewGraphQLServiceProvider::bootGraphQLTypes();
+        ReviewGraphQLServiceProvider::bootGraphQLSchema();
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        //
+	}
+}
