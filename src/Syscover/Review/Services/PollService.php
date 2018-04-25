6e7ae36801da6e9e7cec40e5b4cda669e19c1f83
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-04-25 23:20:31 +0200

    add poll service

--- src/Syscover/Review/Services/PollService.php
+++ src/Syscover/Review/Services/PollService.php
@@ -4,38 +4,48 @@ use Syscover\Review\Models\Poll;
 
 class PollService
 {
-    /**
-     * @param array     $object     contain properties of poll
-     * @return \Syscover\Review\Models\Poll
-     */
     public static function create($object)
     {
-        if($object['default_high_score'] === null) unset($object['default_high_score']);
-        if($object['mailing_days'] === null) unset($object['mailing_days']);
-        if($object['expiration_days'] === null) unset($object['expiration_days']);
-
-        return Poll::create($object);
+        return Poll::create(PollService::builder(
+            PollService::check($object)
+        ));
     }
 
-    /**
-     * @param array     $object     contain properties of poll
-     * @return \Syscover\Review\Models\Poll
-     */
     public static function update($object)
     {
+        Poll::where('id', $object['id'])->update(PollService::builder(
+            PollService::check($object)
+        ));
+
+        return Poll::find($object['id']);
+    }
+
+    private static function builder($object)
+    {
         $object = collect($object);
+        $data = [];
 
-        Poll::where('id', $object->get('id'))->update([
-            'name'                  => $object->get('name'),
-            'email_template'        => $object->get('email_template'),
-            'send_notification'     => $object->get('send_notification'),
-            'poll_route'            => $object->get('poll_route'),
-            'validate'              => $object->get('validate'),
-            'default_high_score'    => $object->get('default_high_score', 5),
-            'mailing_days'          => $object->get('mailing_days', 0),
-            'expiration_days'       => $object->get('expiration_days', 30)
-        ]);
+        if($object->has('name'))                $data['name'] = $object->get('name');
+        if($object->has('email_template'))      $data['email_template'] = $object->get('email_template');
+        if($object->has('poll_route'))          $data['poll_route'] = $object->get('poll_route');
+        if($object->has('send_notification'))   $data['send_notification'] = $object->get('send_notification');
+        if($object->has('validate'))            $data['validate'] = $object->get('validate');
+        if($object->has('default_high_score'))  $data['default_high_score'] = $object->get('default_high_score');
+        if($object->has('mailing_days'))        $data['mailing_days'] = $object->get('mailing_days');
+        if($object->has('expiration_days'))     $data['expiration_days'] = $object->get('expiration_days');
 
-        return Poll::find($object->get('id'));
+        return $data;
+    }
+
+    private static function check($object)
+    {
+        if(empty($object['name'])) throw new \Exception('You have to define a name field to create a poll');
+
+        // delete index, this values has default values
+        if($object['default_high_score'] === null)  unset($object['default_high_score']);
+        if($object['mailing_days'] === null)        unset($object['mailing_days']);
+        if($object['expiration_days'] === null)     unset($object['expiration_days']);
+
+        return $object;
     }
 }
