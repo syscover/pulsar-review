ab24708441774b433d8c24f81e52f2462964422a
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-07-29 11:03:32 +0200

    add new pagination

--- src/Syscover/Review/GraphQL/Types/ReviewPaginationType.php
+++ src/Syscover/Review/GraphQL/Types/ReviewPaginationType.php
@@ -2,6 +2,7 @@
 
 use GraphQL;
 use GraphQL\Type\Definition\Type;
+use Illuminate\Support\Facades\DB;
 use Syscover\Core\Services\SQLService;
 use Folklore\GraphQL\Support\Type as GraphQLType;
 
@@ -36,14 +37,33 @@ class ReviewPaginationType extends GraphQLType
         ];
     }
 
+    public function resolveTotalField($object)
+    {
+        $total = SQLService::countPaginateTotalRecords($object->query);
+
+        // to count elements, if sql has a groupBy statement, count function always return 1
+        // check if total is equal to 1, execute FOUND_ROWS() to guarantee the real result
+        // https://github.com/laravel/framework/issues/22883
+        // https://github.com/laravel/framework/issues/4306
+        return $total === 1 ? DB::select(DB::raw("SELECT FOUND_ROWS() AS 'total'"))[0]->total : $total;
+    }
+
     public function resolveObjectsField($object, $args)
     {
+        // get query filtered by sql statement and filterd by filters statement
+        $query = SQLService::getQueryFiltered($object->query, $args['sql'] ?? null, $args['filters'] ?? null);
+
         // get query ordered and limited
-        $query = SQLService::getQueryOrderedAndLimited($object->query, $args['sql']);
+        $query = SQLService::getQueryOrderedAndLimited($query, $args['sql'] ?? null);
 
         // get objects filtered
         $objects = $query->get();
 
         return $objects;
+    }
+
+    public function resolveFilteredField()
+    {
+        return DB::select(DB::raw("SELECT FOUND_ROWS() AS 'filtered'"))[0]->filtered;
     }
 }
