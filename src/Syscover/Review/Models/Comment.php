57d070cc19600cc05ccbb2288d314b09cdc3744d
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2019-01-11 10:56:03 +0100

    - return null if get data attribute is null

--- src/Syscover/Review/Models/Comment.php
+++ src/Syscover/Review/Models/Comment.php
@@ -39,6 +39,6 @@ class Comment extends CoreModel
     {
         // https://es.wikipedia.org/wiki/ISO_8601
         // return (new Carbon($value))->toW3cString();
-        return (new Carbon($value))->format('Y-m-d\TH:i:s');
+        return $value ? (new Carbon($value))->format('Y-m-d\TH:i:s') : null;
     }
 }
